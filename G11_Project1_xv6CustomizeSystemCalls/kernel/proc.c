71afba1c77c9f92122e2892ee8646264483842a2
Aravind Linga <aravindlinga12@gmail.com>
2026-04-11 22:05:42 +0530

    ipc: initialize message box in process lifecycle

--- G11_Project1_xv6CustomizeSystemCalls/kernel/proc.c
+++ G11_Project1_xv6CustomizeSystemCalls/kernel/proc.c
@@ -146,6 +146,14 @@ found:
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
 
+  // Initialize IPC message box
+  initlock(&p->mbox.lock, "msgbox");
+  p->mbox.active = 0;
+  p->mbox.head = 0;
+  p->mbox.tail = 0;
+  p->mbox.count = 0;
+  memset(p->mbox.name, 0, MSGBOX_NAME);
+
   return p;
 }
 
@@ -179,6 +187,13 @@ freeproc(struct proc *p)
   p->killed = 0;
   p->xstate = 0;
   p->state = UNUSED;
+
+  // Reset IPC message box
+  p->mbox.active = 0;
+  p->mbox.head = 0;
+  p->mbox.tail = 0;
+  p->mbox.count = 0;
+  memset(p->mbox.name, 0, MSGBOX_NAME);
 }
 
 // Free a process slot for syscall helpers.

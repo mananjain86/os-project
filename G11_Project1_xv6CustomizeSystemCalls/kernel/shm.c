25e3b5e24a864df7e8873787fdae05692634c152
kurapati-mahendra <24je0644@iitism.ac.in>
2026-04-14 17:27:09 +0530

    Commit 2: Implement sys_shmcreate

--- G11_Project1_xv6CustomizeSystemCalls/kernel/shm.c
+++ G11_Project1_xv6CustomizeSystemCalls/kernel/shm.c
@@ -20,3 +20,38 @@ void shminit(void) {
     shm_table[i].ref_count = 0;
   }
 }
+
+uint64 sys_shmcreate(void) {
+  int key;
+  argint(0, &key);
+
+  acquire(&shm_lock);
+
+  // Check if segment already exists
+  for (int i = 0; i < SHM_MAX; i++) {
+    if (shm_table[i].key == key && shm_table[i].pa != 0) {
+      release(&shm_lock);
+      return i; // Return shmid
+    }
+  }
+
+  // Find an empty slot
+  for (int i = 0; i < SHM_MAX; i++) {
+    if (shm_table[i].pa == 0) {
+      uint64 pa = (uint64)kalloc();
+      if (pa == 0) {
+        release(&shm_lock);
+        return -1; // Out of memory
+      }
+      memset((void*)pa, 0, PGSIZE);
+      shm_table[i].key = key;
+      shm_table[i].pa = pa;
+      shm_table[i].ref_count = 0; // It will be incremented on shmat
+      release(&shm_lock);
+      return i; // Return shmid
+    }
+  }
+
+  release(&shm_lock);
+  return -1; // Table full
+}
